a0c1434399371a4227cfbc5c398ff28dcc45ff22
ridho <ridhopratamavd@gmail.com>
2024-02-17 11:38:18 +0700

    addd virtual thread

---
+++ virtualthread/src/main/java/com/btpn/vcs/virtualthread/BookModel.java
@@ -0,0 +1,26 @@
+package com.btpn.vcs.virtualthread;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "books")
+@Builder
+@Data // rest 
+@AllArgsConstructor // db
+@NoArgsConstructor // db
+public class BookModel implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    Long id;
+
+    String title;
+}
